bccc38edbbd0f88c19b5a2d461bdb9040eec575c
SCatella <samuel.catella@gmail.com>
2021-12-19 02:08:26 -0700

    Cleaning up depricated files and comments

--- script.js
+++ script.js
@@ -60,37 +60,3 @@ color1.addEventListener("input", setGradient);
 color2.addEventListener("input", setGradient);
 
 randButton.addEventListener("click", randGradient);
-
-
-/* Save the Random Hex Number Generator here for later
-
-//Generate Random number between 0 and 15.
-const randNum = () => {
-    const number = Math.floor(Math.random() * 15);
-    return number;
-}
-
-// Take the randNum and format to hex values and return values as strings.
-const hexNum = () => {
-    let number = randNum();
-    switch (number) {
-        case 10: return 'a';
-        case 11: return 'b';
-        case 12: return 'c';
-        case 13: return 'd';
-        case 14: return 'e';
-        case 15: return 'f';
-        default: return number.toString();
-    }
-}
-
-// Take hexNum and generate a HexColor value (as a string that starts with #).
-const hexColor = () => {
-    let colorValue = '#';
-    for (let i = 0; i < 6; i++) {
-        hexColor.concat(hexNum());
-    }
-    return colorValue;
-}
-
-*/
